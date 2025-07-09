c46e8ceb5c006c0f0589307f5d0ee65b5f55019a
Evgeny Moerman <moerman@fhi-berlin.mpg.de>
2025-07-09 11:27:49 +0200

    Implement individual distance normalization for each element type

--- pysages/colvars/coordinates.py
+++ pysages/colvars/coordinates.py
@@ -202,7 +202,6 @@ class CoordinationNumber(TwoPointCV):
         self.r0_table_species = self.extract_r0_for_species(self.radii_table_default, self.species)
         #self.indices_nn = self.nbrs.idx[1][np.where(np.isin(self.nbrs.idx[0], self.indices[1]))]
         self.r0_lookup_table, self.element_to_index, self.element_ids_array, self.particle_to_lookup_idx = self.create_r0_lookup_table(self.r0_table_species, self.species)
-        #print(f'indices_nn : {self.indices_nn}')
         #Estimate max number of neighbors from the current neighborlist; multiply by 1.5 to account for potential fluctuations 
         #self.number_nn_max = np.minimum( int(len(self.nbrs.idx[1][np.where(np.isin(self.nbrs.idx[0], indices[1][0]))]) * 1.5), len(species) )
         self.number_nn_max = np.minimum( int(self.determine_max_neighbors(self.nbrs, self.indices_cn) * 4.0), len(self.species) )
@@ -219,7 +218,6 @@ class CoordinationNumber(TwoPointCV):
         n = len(particle_idxs)
         num_neighbors = np.zeros(n, dtype=np.int32)
         num_neighbors = jax.lax.fori_loop(0, n, determine_num_neighbors, num_neighbors)
-        #jdb.print('num_neighbors_list = {num_neighbors}', num_neighbors=num_neighbors)
         #Return the biggest number of neighbors
         return np.max(num_neighbors)
 
@@ -251,27 +249,24 @@ class CoordinationNumber(TwoPointCV):
             r0_table = r0_table.at[j, i].set(r0_val)
 
         element_ids_array = np.array(unique_elements)
-
+        index_lookup = np.full(np.max(element_ids_array)+1, -1, dtype=np.int32).at[element_ids_array].set(np.arange(0,element_ids_array.size))
         #return r0_table, element_to_index, element_ids_array
-        return r0_table, element_to_index, element_ids_array, jax.vmap(get_lookup_index)(species) 
+        return r0_table, element_to_index, element_ids_array, index_lookup 
 
     @property
     def function(self):
         #return lambda r_all, r_cn: calculate_coordination_number(r_all, r_cn, self.nbrs, self.species, self.indices, self.number_nn)
-        return lambda r_all, r_cn: calculate_coordination_number(self.nbrs, self.indices_cn, r_all, self.number_nn_max, self.r0_table_species, self.species, self.species_nn, self.box)
+        return lambda r_all, r_cn: calculate_coordination_number(self.nbrs, self.indices_cn, r_all, self.number_nn_max, self.r0_lookup_table, self.particle_to_lookup_idx, self.species, self.species_nn, self.box)
 
-def calculate_coordination_number(edge_list_obj, indices_cn, all_positions, max_neighbors, r0_dict, all_species, species_nn, box):
+def calculate_coordination_number(edge_list_obj, indices_cn, all_positions, max_neighbors, r0_dict, element_to_local_index,all_species, species_nn, box):
 #def calculate_coordination_number(edge_list, indices_cn, all_positions, max_neighbors, r0_dict, all_species, species_nn):
     n = len(indices_cn)
     N = all_positions.shape[0]
 
-    #jdb.print('max_neighbors = {max_neighbors}', max_neighbors=max_neighbors)
     #Update neighborlist
     edge_list_obj = edge_list_obj.update(all_positions, neighbor=edge_list_obj.idx, box=box)
     edge_list = edge_list_obj.idx
-    #jdb.print('edge_list : {edge_list}', edge_list=edge_list)
 
-    #jdb.print(edge_list)
     def get_all_neighbors():
         # Create boolean mask for edges originating from our particles
         #particle_mask = np.isin(edge_list[0], indices_cn)
@@ -287,7 +282,6 @@ def calculate_coordination_number(edge_list_obj, indices_cn, all_positions, max_
 
             #Collect all unique neighbors for particle i
             unique_targets_to_particle_i = np.unique(np.where(edge_list[1] == particle_idx, edge_list[0], -1), size=max_neighbors, fill_value=-1)
-            #jdb.print('unique targets : {unique_targets_to_particle_i}', unique_targets_to_particle_i=unique_targets_to_particle_i)
 
             mask = (unique_targets_to_particle_i >= 0) & (unique_targets_to_particle_i != particle_idx)
             if species_nn is not None:
@@ -332,18 +326,27 @@ def calculate_coordination_number(edge_list_obj, indices_cn, all_positions, max_
     # Mask invalid distances
     masked_distances = np.where(valid_mask, distances, np.nan)
 
+    def normalize_distances(distances, r0_table, element_ids_center, element_ids_neighbors):
+
+        reference_distances = r0_table[element_ids_center[:,None], element_ids_neighbors]
+        #jdb.print('Reference distances : {reference_distances}', reference_distances=reference_distances)
+        return distances/reference_distances
+
     # Compute coordination numbers
-    normalized_distances = masked_distances / 1.7
+    #normalized_distances = masked_distances / 1.7
+    element_ids_center = element_to_local_index[all_species[indices_cn]]
+    element_ids_neighbors = element_to_local_index[all_species[safe_neighbor_indices]]
+    normalized_distances = normalize_distances(masked_distances, r0_dict, element_ids_center, element_ids_neighbors)
     mask = ~np.isnan(normalized_distances)
-    numerator = 1.0 - normalized_distances**6
-    denominator = 1.0 - normalized_distances**12
+    #numerator = 1.0 - normalized_distances**6
+    #denominator = 1.0 - normalized_distances**12
 
     cn_terms = np.where(mask, (1.0 - normalized_distances**6)/(1.0 - normalized_distances**12), 0.0)
     cn_value = np.sum(cn_terms)
 
     #jdb.print('CN : {cn_value}', cn_value=cn_value)
     #jdb.print('NH2 coordinates : {pos}', pos=all_positions[np.array([1567, 1568, 1569])])
-    #jdb.print('all_neighbor_indices : {all_neighbor_indices}', all_neighbor_indices=all_neighbor_indices)
-    #jdb.print('Masked distances : {masked_distances}', masked_distances=masked_distances)
+    #jdb.print('all_neighbor_indices : {all_neighbor_indices}', all_neighbor_indices=all_species[safe_neighbor_indices])
+    #jdb.print('Masked distances : {masked_distances}', masked_distances=normalized_distances)
     #jdb.breakpoint()
     return cn_value
